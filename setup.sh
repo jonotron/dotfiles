4d02f7a740dcc9dee6822dda91f52fc6b0e803fc
Jonathan Bowers <jonotron@gmail.com>
2011-06-02 12:11:12 -0700

    Added setup to get started

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# setup git
+git config --global user.name "Jonathan Bowers"
+git config --global user.email "jonotron@gmail.com"
+git config --global color.ui auto
+
+# remove old configs
+rmdir ~/.vim
+rm ~/.vimrc
+rm ~/.bash_profile
+
+# symlink the new files
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.vim ~/.vim
+ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
