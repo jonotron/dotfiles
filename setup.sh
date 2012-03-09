101f2bdeef011d9790e4c1db9c44da052ebe4e51
Jonathan Bowers <jonotron@gmail.com>
2012-03-09 09:39:39 -0800

    .gitconfig

--- setup.sh
+++ setup.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-# setup git
-git config --global user.name "Jonathan Bowers"
-git config --global user.email "jonotron@gmail.com"
-git config --global color.ui auto
 
 # remove old configs
 rmdir ~/.vim
 rm ~/.vimrc
 rm ~/.bash_profile
+rm ~/.gitconfig
 
 # symlink the new files
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
@@ -15,3 +12,4 @@ ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/ackrc ~/.ackrc
 ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
