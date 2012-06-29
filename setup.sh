e658c60d763578ff0dbba3092f10d0d028aa108a
Jonathan Bowers <jonotron@gmail.com>
2012-06-29 09:43:40 -0700

    Added .zshrc

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ rmdir ~/.vim
 rm ~/.vimrc
 rm ~/.bash_profile
 rm ~/.gitconfig
+rm ~/.zshrc
 
 # symlink the new files
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
@@ -16,3 +17,4 @@ ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/ackrc ~/.ackrc
 ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
