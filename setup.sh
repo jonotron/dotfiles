3e71d83244a3779bb961e15d64b4a98a656f1618
Jonathan Bowers <jonotron@gmail.com>
2012-06-29 09:38:06 -0700

    Added oh-my-zsh install

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# install oh-my-zsh
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+
 # remove old configs
 rmdir ~/.vim
 rm ~/.vimrc
